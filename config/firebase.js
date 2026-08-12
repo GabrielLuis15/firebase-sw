368dce627636161f3f1d022d06b116e3e2396b82
Gabriel Luis de Andrade Sousa <gab31012010@gmail.com>
2026-08-12 19:54:48 -0400

    chaves do vercel

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,34 @@
 // CONFIGURAÇÃO FIREBASE REALTIME DATABASE
+
 const admin = require("firebase-admin");
 
-// Renomeie o arquivo serviceAccountKey-exemplo.json
-// para serviceAccountKey.json e coloque suas credenciais do Firebase.
 
-const serviceAccount = require("./serviceAccountKey.json");
+// Se as variáveis de ambiente existirem,
+// significa que estamos no Vercel/produção.
+if (process.env.FIREBASE_PROJECT_ID) {
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://sistemaswebatividade-default-rtdb.firebaseio.com/"
-});
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        }),
+
+        databaseURL: "https://sistemaswebatividade-default-rtdb.firebaseio.com/"
+    });
+
+} else {
+
+    // Ambiente local
+    const serviceAccount = require("./serviceAccountKey.json");
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+
+        databaseURL: "https://sistemaswebatividade-default-rtdb.firebaseio.com/"
+    });
+}
+
 
 const db = admin.database();
 
