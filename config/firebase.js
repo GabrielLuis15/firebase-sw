c310ae5bb84017d95bce1a1dd26d83adef170113
Gabriel Luis de Andrade Sousa <gab31012010@gmail.com>
2026-08-18 18:02:35 -0300

    acerto de contas

--- config/firebase.js
+++ config/firebase.js
@@ -29,6 +29,27 @@ if (process.env.FIREBASE_PROJECT_ID) {
     });
 }
 
+const admin = require('firebase-admin');
+
+let serviceAccount;
+
+// Se estiver rodando na Vercel (onde existe a variável de ambiente)
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Se estiver rodando localmente na sua máquina
+  serviceAccount = require('./serviceAccountKey.json');
+}
+
+// Inicializa o Firebase Admin
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+
+module.exports = admin;
+
 
 const db = admin.database();
 
